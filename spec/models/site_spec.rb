9e1af1062137a584663f7ecaf129a5b709a455a9
Sean McEvoy <sfmcevoy@gmail.com>
2023-07-13 15:49:50 -0500

    Add spec coverage to verify foreign key constraints

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -3,4 +3,13 @@ require "rails_helper"
 RSpec.describe Site, type: :model do
   it { should belong_to(:account) }
   it { should have_many(:pages) }
+
+  it "is destroyed when parent account is destroyed" do
+    account = Account.create!
+    site = account.sites.create!
+
+    Account.delete_all
+
+    expect { Site.find(site.id) }.to raise_exception(ActiveRecord::RecordNotFound)
+  end
 end
